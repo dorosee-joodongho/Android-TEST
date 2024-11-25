8ac683b087d975c338281a551e2c7cc4a82b4c51
KimBoYoung <qhdud20@naver.com>
2024-11-25 18:13:59 +0900

    11/25 로그인 통신 연결, 라우터 수정

--- app/src/main/java/com/example/myapplication/network/RetrofitApi.kt
+++ app/src/main/java/com/example/myapplication/network/RetrofitApi.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication.network
 
 import android.telecom.Call
+import com.example.myapplication.data.LoginResponse
 import com.example.myapplication.data.PostJoinRequestDto
 import com.example.myapplication.data.PostJoinResponseDto
+import com.example.myapplication.data.PostLoginRequestDto
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -10,4 +12,22 @@ import retrofit2.http.POST
 interface RetrofitApi {
     @POST("join")
     suspend fun join(@Body postJoinRequestDto: PostJoinRequestDto): PostJoinResponseDto
+
+    @POST("login")
+    suspend fun login(@Body postLoginRequestDto: PostLoginRequestDto): LoginResponse
 }
+
+
+/*
+    @GET("items")
+    fun getItems(): Call<List<Item>>
+
+    @POST("items")
+    fun createItem(@Body item: Item): Call<Item>
+
+    @PUT("items/{id}")
+    fun updateItem(@Path("id") id: Int, @Body item: Item): Call<Item>
+
+    @DELETE("items/{id}")
+    fun deleteItem(@Path("id") id: Int): Call<Void>
+ */
